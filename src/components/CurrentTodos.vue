1c56dffc61ecd2eead13e546c354ff6fccf1936b
任思强 <rensiqiang@2144.cn>
2018-05-25 17:06:26 +0800

    vuex todo demo

---
+++ src/components/CurrentTodos.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="current-todos">
+        <h3 v-if="todos.length > 0">当前项目({{todos.length}})</h3>
+        <ul class="list-group">
+            <li class="list-group-item" v-for="todo in todos">
+                {{todo.body}}
+                <div class="btn-group">
+                    <button type="button" @click="edit(todo)" class="btn btn-default btn-sm">
+                    <span class="glyphicon glyphicon-edit"></span> 编辑
+                    </button>
+                    <button type="button" @click="complete(todo)" class="btn btn-default btn-sm">
+                    <span class="glyphicon glyphicon-ok-circle"></span> 完成
+                    </button>
+                    <button type="button" @click="remove(todo)" class="btn btn-default btn-sm">
+                    <span class="glyphicon glyphicon-remove-circle"></span> 删除
+                    </button>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default{
+    methods: {
+        edit(todo){
+            this.$store.dispatch('editTodo', todo)
+        },
+        complete(todo){
+            this.$store.dispatch('completeTodo', todo)
+        },
+        remove(todo){
+            this.$store.dispatch('removeTodo', todo)
+        }
+    },
+    computed: {
+        todos(){
+            return this.$store.getters.todos
+        }
+    }
+}
+</script>
+<style>
+    .btn-group{
+        float: right;
+    }
+    h3{
+      margin:15px 0;
+    }
+</style>
---
+++ src/components/GetTodo.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="get-todo">
+        <input class="form-control"
+               :value="newTodo"
+               @change="getTodo"
+               placeholder="输入项目...">
+        <button class="btn btn-primary" @click="addTodo">添加项目</button>
+    </div>
+</template>
+<script>
+    export default{
+        methods: {
+            getTodo(e){
+                this.$store.dispatch('getTodo', e.target.value)
+            },
+            addTodo(){
+                this.$store.dispatch('addTodo')
+                this.$store.dispatch('clearTodo')
+            }
+        },
+        computed: {
+            newTodo(){
+                return this.$store.getters.newTodo
+            }
+        }
+    }
+</script>
+
+<style>
+    #get-todo{
+      margin-top: 15px;
+    }
+    .btn-primary{
+      margin-top: 5px;
+    }
+</style>
